5557a2152c3bd2fce026f310b815fb758c2d626f
alimran halmamat <mr.uygur1996@gmail.com>
2021-10-23 18:52:08 -0700

    TicTacToe

--- TikTacToe/TicTacToe.java
+++ TikTacToe/TicTacToe.java
@@ -1,113 +1,120 @@
+// import scanner
 import java.util.Scanner;
 
 public class TicTacToe {
+    public static Scanner input = new Scanner(System.in);
 
+/* 
+	 * Main Method for Final Step - Delete your main method 
+	 * and uncomment this one. 
+	 * Runs the game by getting input from the user, making the 
+	 * appropriate moves, and prints who won or if it was a stalemate. 
+	*/ 
     public static void main(String[] args) {
+        char[][] board = new char[3][3];
+        displayBoard(board);
 
+        while (true) {
+            // Prompt the first player
+            makeAMove(board, 'X');
+
+            //check if the winner is player X
+            displayBoard(board);
+            if (isWon('X', board)) {
+                System.out.println("X player won");
+                System.exit(1);
+            } else if (isDraw(board)) {
+                System.out.println("Stalemate");
+                System.exit(2);
+            }
+            /*
+	        * Switches the current player from X to O, or O to X.
+	        */
+            // Prompt the second player
+            makeAMove(board, 'O');
+            displayBoard(board);
+            //check if the winner is player O
+            if (isWon('O', board)) {
+                System.out.println("O player won");
+                System.exit(3);
+            } else if (isDraw(board)) {
+                System.out.println("Stalemate");
+                System.exit(4);
+            }
+        }
     }
 
-    private char[][] board;
-    private char player; // 'X' or 'O'
+    public static void makeAMove(char[][] board, char player) {
+        boolean done = false;
 
-    /*
-     * Instantiate board to be a 3 by 3 char array of spaces. Set player to be 'X'.
-     */
-    public TicTacToe() {
-        /*
-         * Step 1: create an empty board, with an initial value of a space (' ')
-         */
+        do {
+            System.out.print("Enter a row for player " + player + ": ");
+            int row = input.nextInt() - 1;
+            if(row > 2){
+                System.out.println("out of range");
+                continue;
+            }
+            System.out.print("Enter a column for player " + player + ": ");
+            int column = input.nextInt() - 1;
+            if(column > 2){
+                System.out.println("out of range");
+                continue;
+            }
+            if (board[row][column] == '\u0000') { // an empty cell
+                board[row][column] = player;
+                done = true;
+            } else
+                System.out.println("This cell is already occupied. Try a different cell");
+        } while (!done);
     }
 
+    /* 
+	 * Instantiate board to be a 3 by 3 char array of spaces.
+	 * Set player to be 'X'.
+	 */
+    static void displayBoard(char[][] board) {
+        System.out.println("\n-------------");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++)
+                System.out.print(board[i][j] != '\u0000' ? board[i][j] + " | " : "  | ");
+            System.out.println("\n-------------");
+        }
+    }
     /*
-     * If s represents a valid move, add the current player's symbol to the board
-     * and return true. Otherwise return false.
-     */
-    public boolean play(String s) {
-        /*
-         * Step 2: Fill in here with your own play logic, and replace the return with
-         * you own.
-         */
+	 * Returns true if the current player has won the game.
+	 * Three in a row, column or either diagonal.
+	 * Otherwise, return false.
+	 */
+    public static boolean isWon(char ch, char[][] board) {
+        // Check rows
+        for (int i = 0; i < 3; i++)
+            if (ch == board[i][0] && ch == board[i][1] && ch == board[i][2])
+                return true;
+
+        // Check columns
+        for (int j = 0; j < 3; j++)
+            if (ch == board[0][j] && ch == board[1][j] && ch == board[2][j])
+                return true;
+
+        // Check major diagonal
+        if (ch == board[0][0] && ch == board[1][1] && ch == board[2][2])
+            return true;
+
+        // Check sub diagonal
+        if (ch == board[0][2] && ch == board[1][1] && ch == board[2][0])
+            return true;
+
         return false;
     }
 
-    /*
-     * Switches the current player from X to O, or O to X.
-     */
-    public void switchTurn() {
-        // Step 3: Fill in with your code to toggle between
-        // 'X' and 'O'
+    public static boolean isDraw(char[][] board) {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (board[i][j] == '\u0000')
+                    return false;
+
+        return true; // All cells are now occupied
     }
-
-    /*
-     * Returns true if the current player has won the game. Three in a row, column
-     * or either diagonal. Otherwise, return false.
-     */
-    public boolean won() {
-        /*
-         * Step 5: Fill in the code for the won method. This method should return true
-         * if the current player has 3 in-a-row in any row, column or diagonal.
-         * Otherwise, return false.
-         */
-        return false; // TODO: replace with your own return statement.
-    }
-
-    /*
-     * Returns true if there are no places left to move
-     */
-    public boolean stalemate() {
-        /*
-         * Step 4: Fill in the code for the stalemate method. It should return true if
-         * there are no more places to move on the board. Otherwise, return false return
-         * false;
-         */
-        return true; // replace with your own return
-    }
-
-    public char getPlayer() {
-        return player;
-    }
-
-    public void print() {
-        System.out.println();
-        System.out.println("\t  1 2 3");
-        System.out.println();
-        System.out.println("\tA " + board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
-        System.out.println("\t  -----");
-        System.out.println("\tB " + board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
-        System.out.println("\t  " + "-----");
-        System.out.println("\tC " + board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
-        System.out.println();
-    }
-
-    /*
-     * Step 6: Main Method for Final Step - Delete your main method and uncomment
-     * this one. Runs the game by getting input from the user, making the
-     * appropriate moves, and prints who won or if it was a stalemate.
-     */
-    /*
-     * public static void main(String[] args) {
-     * 
-     * Scanner in = new Scanner(System.in); TicTacToe game = new TicTacToe();
-     * System.out.println("Welcome to tic-tac-toe"); System.out.
-     * println("Enter coordinates for your move following the X and O prompts");
-     * 
-     * while(!game.stalemate()) { //Print the game
-     * 
-     * 
-     * //Prompt player for their move
-     * 
-     * 
-     * //Loop while the method play does not return true when given their move.
-     * //Body of loop should ask for a different move
-     * 
-     * //If the game is won, call break;
-     * 
-     * 
-     * //Switch the turn
-     * 
-     * } game.print(); if(game.won()){
-     * System.out.println("Player "+game.getPlayer()+" Wins!!!!"); } else {
-     * System.out.println("Stalemate"); } }
-     */
-
 }
